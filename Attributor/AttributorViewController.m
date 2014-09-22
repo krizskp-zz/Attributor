dccb62e73522bef3012774492829ce2f7b9811b6
krizskp <shalvindra@outlook.com>
2014-09-23 11:02:29 +1200

    Added button handlers for outline and unoutline text.

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -15,16 +15,27 @@
 
 @implementation AttributorViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
 	[self.body.textStorage addAttribute:NSForegroundColorAttributeName
 								  value:sender.backgroundColor
 								  range:self.body.selectedRange];
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (IBAction)outlineBodySelection {
+	[self.body.textStorage addAttributes:@{NSStrokeWidthAttributeName : @-3,
+										   NSStrokeColorAttributeName : [UIColor blackColor]}
+								   range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection {
+	[self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+									 range:self.body.selectedRange];
 }
 
 @end
