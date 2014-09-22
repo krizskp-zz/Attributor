33156a4eeffc7bac45aec44bd3dd4d648f3d97de
krizskp <shalvindra@outlook.com>
2014-09-23 11:23:31 +1200

    Added viewDidload event to set attributes

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -11,6 +11,7 @@
 @interface AttributorViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *headline;
 @property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @end
 
 @implementation AttributorViewController
@@ -19,6 +20,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	NSMutableAttributedString *title =
+		[[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+	[title setAttributes:@{NSStrokeWidthAttributeName : @3,
+						   NSStrokeColorAttributeName : self.outlineButton.tintColor}
+				   range:NSMakeRange(0, [title length])];
+	[self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
